67949442cfd42c67032ccf5cffa70009777aaccf
NiasVl <nicovieira88@gmail.com>
2025-08-13 13:50:24 -0300

    funcionando

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -20,31 +20,31 @@ export default function App() {
 
     async function buscarFilmes() {
       const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMmNkYTUxYzRjYzljZDdiZTJjMzMzMDE3ZGM3OWE0NCIsIm5iZiI6MTc1NTAyMTYwNS44OTY5OTk4LCJzdWIiOiI2ODliODEyNTY2NTBjYzA1YWFjNzAwNjciLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.i0wiHhy0MurWY0nId6NgI5gRpCipGKoBtGi5hpPVlT8'
-  }
-};
-
-  fetch(url, options)
-    .then(res => res.json())
-    .then(json => {
-      let data = json
-      return(data)})
-
-
-
-        setMovies(data)
-
-        console.log(movies)
-
-    }
-
-    buscarFilmes();
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMmNkYTUxYzRjYzljZDdiZTJjMzMzMDE3ZGM3OWE0NCIsIm5iZiI6MTc1NTAyMTYwNS44OTY5OTk4LCJzdWIiOiI2ODliODEyNTY2NTBjYzA1YWFjNzAwNjciLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.i0wiHhy0MurWY0nId6NgI5gRpCipGKoBtGi5hpPVlT8'
+        }
+      };
+      
+      fetch(url, options)
+        .then(res => res.json())
+        .then(json => {
+          let data = json.results
+          console.log(data)
+          
+          setMovies(data)
+        })
+        
+        
+      }
+      
+      buscarFilmes()
+      
 
   }, [])
+
 
 
   return (
@@ -69,7 +69,7 @@ const options = {
       data ={movies}
       horizontal = {true}
       showsHorizontalScrollIndicator={false}
-      keyExtractor={(item) => item.id}
+      reyExtractor={(item) => item.id}
       renderItem= {({item}) => (
 
         <CardMovies 
